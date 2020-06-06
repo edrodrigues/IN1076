6a8b1c8d1339038f34977bf693ff4706c6e6495c
Edmilson Rodrigues <ed.ufpe@gmail.com>
2020-06-06 10:49:23 -0300

    Exercicio Trivia Star Wars - Aula 3

---
+++ Aula 3.py
@@ -0,0 +1,37 @@
+# Trivia StarWars - 5 perguntas
+
+def trivia():
+    Pergunta1 = "Em que ano foi lançado o primeiro filme de Star Wars no cinema?"
+    RespostaPergunta1 = "1977"
+
+    Pergunta2 = "Qual o nome do mestre Jedi que é verde e pequeno?"
+    RespostaPergunta2 = "Yoda"
+
+    Pergunta3 = "Qual o primeiro nome do protagonista da trilogia original de Star Wars?"
+    RespostaPergunta3 = "Luke"
+
+    Pergunta4 = "O nome do vilão que se veste de preto e tem uma voz metálica é Darth ..."
+    RespostaPergunta4 = "Vader"
+
+    Pergunta5 = "Escreva a letra da opção que NÃO é o nome de um título de filme da saga Star Wars. /n A) Uma nova Esperança /n B) O Retorno do Jedi /n C) Os Jedis Contra Atacam "
+    RespostaPergunta5 = "C"
+
+# Funções Fazer Perguntas
+    fazerPergunta1(Pergunta1, RespostaPergunta1)
+    fazerPergunta2(Pergunta2, RespostaPergunta2)
+    fazerPergunta3(Pergunta3, RespostaPergunta3)
+    fazerPergunta4(Pergunta4, RespostaPergunta4)
+    fazerPergunta5(Pergunta5, RespostaPergunta5)
+
+def fazerPergunta(pergunta, resposta):
+    print(pergunta)
+    palpite = input()
+    pontuação == 0
+    if palpite == resposta:
+        print ("Parabéns! Você é um(a) Nerd Raiz.")
+        pontuação += 1
+    else:
+        print("Errado. Você é um nerd Nutella.")
+
+# Inicia o Jogo
+trivia()
